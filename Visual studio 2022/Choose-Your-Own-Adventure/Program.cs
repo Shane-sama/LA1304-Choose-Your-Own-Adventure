368ca34a3951a7acb53ae12b71ae9309a597945b
Shane-sama <shane.roellin@icloud.com>
2023-03-03 08:21:06 +0100

    structure

--- Visual studio 2022/Choose-Your-Own-Adventure/Program.cs
+++ Visual studio 2022/Choose-Your-Own-Adventure/Program.cs
@@ -1,2 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace Choose_Your_Own_Adventure
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Choose Your Own Adventure!");
+        }
+    }
+}
