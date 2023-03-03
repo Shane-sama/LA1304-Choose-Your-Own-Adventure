a726a9d30a681f215f5ee8f4f3c4117fc3e23465
Shane-sama <shane.roellin@icloud.com>
2023-03-03 09:00:04 +0100

    start

---
+++ Visual studio 2022/Choose-Your-Own-Adventure/Game.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Choose_Your_Own_Adventure
+{
+    public class Game
+    {
+        public static void Adventure()
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
--- Visual studio 2022/Choose-Your-Own-Adventure/Program.cs
+++ Visual studio 2022/Choose-Your-Own-Adventure/Program.cs
@@ -7,6 +7,28 @@ namespace Choose_Your_Own_Adventure
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Choose Your Own Adventure!");
+            try
+            {
+                Console.WriteLine("Do you want to play a game? [yes | no]");
+                string UserInput = Console.ReadLine();
+                string Play = UserInput.ToLower();
+                if (Play == "yes")
+                {
+                    Game.Adventure();
+                }
+                else if (Play == "no")
+                {
+                    Environment.Exit(0);
+                }
+                else
+                {
+                    throw new Exception();
+                }
+            }
+            catch
+            {
+                Console.WriteLine("Please enter a valid awnser!");
+            }
         }
     }
 }
