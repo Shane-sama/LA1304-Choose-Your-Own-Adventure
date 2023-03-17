2e0581164dd234591553062f1c7005432b52d70f
gummyflummy <D.Zumstein.Inf21@stud.bbbaden.ch>
2023-03-17 08:51:52 +0100

    Path 1 storylore siu
    yes

--- Visual studio 2022/Choose-Your-Own-Adventure/Path_1/Path_1.cs
+++ Visual studio 2022/Choose-Your-Own-Adventure/Path_1/Path_1.cs
@@ -5,7 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 using static System.Net.Mime.MediaTypeNames;
 
-namespace Choose_Your_Own_Adventure.Path_1 
+namespace Choose_Your_Own_Adventure.Path_1
 {
     public class Path_1
     {
@@ -15,21 +15,55 @@ namespace Choose_Your_Own_Adventure.Path_1
             Game.Text = "You try to search for another way out. You are walking around and find out that "
                       + "you're in the Mensa building. That's really good news hence there are hallways to "
                       + "two different buildings and there could be food somewhere here. One guides to the "
-                      + "Sporthalls and one to the Biology Building. "
+                      + "Sporthalls and one to the biology building. "
                       + "While you're thinking about which building you want to go and if you want to search for "
                       + "food first, you suddenly get a weird "
                       + "feeling as if someone is watching you. You should go somewhere else than this "
-                      + "place quickly.";
+                      + "place quickly. What do you want to do? ";
 
             Game.Enter = true;
             Game.Typing();
             Game.Enter = false;
-            Game.Text = "1. Look for another way out.";
+            Game.Text = "1. Go to the Sporthalls";
             Game.Typing();
-            Game.Text = "2. Go back to the classroom and try to wait for the morning.";
+            Game.Text = "2. Go to the biology building";
+            Game.Typing();
+            Game.Text = "3. Search for food before going anywhere";
             Game.Typing();
             Game.Text = "[Choose your path!]";
             Game.Typing();
-    }
+            Game.Enter = true;
+
+            while (true)
+            {
+                try
+                {
+                    string Choice = Console.ReadLine();
+
+                    if (Choice == "1")
+                    {
+                        Path_1_1.Sporthalls();
+                        break;
+                    }
+                    else if (Choice == "2")
+                    {
+                        Path_1_2.Biology();
+                        break;
+                    }
+                    else if (Choice == "3")
+                    {
+                        Path_1_3.FoodMensa();
+                    }
+                    else
+                    {
+                        throw new Exception();
+                    }
+                }
+                catch
+                {
+                    Console.WriteLine("Please enter a valid answer!");
+                }
+            }
+        }
     }
 }
---
+++ Visual studio 2022/Choose-Your-Own-Adventure/Path_1/Path_1_3.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Choose_Your_Own_Adventure.Path_1
+{
+    public class Path_1_3
+    {
+        public static void FoodMensa()
+        {
+            Console.WriteLine("goofy");
+        }
+    }
+}
