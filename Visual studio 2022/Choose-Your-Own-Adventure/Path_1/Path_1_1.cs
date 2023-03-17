0f9ef3d244e545ef896e22b06313648f1a7d85fc
gummyflummy <D.Zumstein.Inf21@stud.bbbaden.ch>
2023-03-17 09:42:31 +0100

    path 1.1 story lore siu
    yaam

--- Visual studio 2022/Choose-Your-Own-Adventure/Path_1/Path_1_1.cs
+++ Visual studio 2022/Choose-Your-Own-Adventure/Path_1/Path_1_1.cs
@@ -10,8 +10,16 @@ namespace Choose_Your_Own_Adventure.Path_1
     {
         public static void Sporthalls()
         {
-            Console.WriteLine("goofy");
+            Console.Clear();
+            Game.Text = "You are walking in the hallway to the sporthalls. While you're wallking you hear a "
+                      + "strange loud high pitched scream coming from the Mensa Building you just were. You're lucky"
+                      + "that you didn't need to find out what made that sound. Anyhow, you continue your way to the sporthalls."
+                      + "Right before the stairs to the sporthalls you see alot of apple boxes just laying around. "
+                      + "all the apple boxes have the initial C on them. You think that this is very odd so you rather ignore "
+                      + "the boxes and go up the stairs. You finally reached your destination. The first thing you wanna do "
+                      + "is to look if the doors to the outside are also locked."
         }
 
     }
 }
+ 
