0f78f08a8bc63275b8ac8b9ff945e78a48070516
gummyflummy <D.Zumstein.Inf21@stud.bbbaden.ch>
2023-03-17 07:57:32 +0100

    Korrikatura'ww'
    ja

--- Visual studio 2022/Choose-Your-Own-Adventure/Path_2/Path_2.cs
+++ Visual studio 2022/Choose-Your-Own-Adventure/Path_2/Path_2.cs
@@ -10,13 +10,14 @@ namespace Choose_Your_Own_Adventure.Path_2
     {
         public static void Wait()
         {
+            Console.Clear();
             Game.Text = "You go back to the classroom and try to fall asleep using your jacket "
                 + "as a pillow. But you can't. You aren't tired anymore and there's this weird "
                 + "feeling, as if something is watching. You take out your phone to check the "
                 + "time and to watch some videos or play some games to make the time pass by. "
                 + "You realise there's no internet connection. Seems like the school turns it "
                 + "off during the night. The lights in the room also don't work .They must be "
-                + "turning off the electricity for the whole place. That seems like way to much "
+                + "turning off the electricity for the whole place. That seems like way too much "
                 + "effort you think to yourself.";
             Game.Enter = true;
             Game.Typing();
